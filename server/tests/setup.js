2aa64e2fdf2de53e35521c7f79f08e38f12d8ce5
San4eL0 <aleksandrsilo207@gmail.com>
2026-09-24 09:23:08 +0700

    Update setup.js

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -1,18 +1,11 @@
-/**
- * Настройка тестовой среды.
- * Выполняется один раз перед всеми тестами.
- */
 const { sequelize } = require('../models');
 
 beforeAll(async () => {
-  // Синхронизируем схему в тестовой БД
   await sequelize.sync({ force: true });
 });
 
 afterAll(async () => {
-  // Закрываем соединение, чтобы Jest завершился
   await sequelize.close();
 });
 
-// Увеличиваем таймаут для медленных операций
 jest.setTimeout(30000);
